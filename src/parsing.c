abe7db1c56df8614db0e03920644a6484e9486d0
nojia1775 <125820548+nojia1775@users.noreply.github.com>
2024-05-03 10:30:51 +0000

    jsuis trop chaud

--- src/parsing.c
+++ src/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nadjemia <nadjemia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 12:52:04 by nadjemia          #+#    #+#             */
-/*   Updated: 2024/05/02 14:38:41 by nadjemia         ###   ########.fr       */
+/*   Updated: 2024/05/03 10:19:32 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	parsing(t_input	*input)
 	if (!result)
 		return (printf("minishell : Syntax error\n"), 0);
 	printf("%s\n", result);
-	//printf("%zu\n", mini_split(input->intput_brut));
 	input->cmd = result;
 	return (1);
 }
